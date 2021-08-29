72975f67225fc84ad53a0dd571423cc6a96e0aea
MatheusMoraes66 <matheus.csharp@hotmail.com>
2021-08-29 01:08:02 -0300

    implement restTemplate

---
+++ hr-payroll/src/main/java/com/matheus/hrpayroll/enteties/Worker.java
@@ -0,0 +1,63 @@
+package com.matheus.hrpayroll.enteties;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+
+public class Worker implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+
+    private Long id;
+
+    private String name;
+
+    private Double dailyIncome;
+
+    public Worker() {
+    }
+
+    public Worker(Long id, String name, Double dailyIncome) {
+        this.id = id;
+        this.name = name;
+        this.dailyIncome = dailyIncome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Worker)) return false;
+        Worker worker = (Worker) o;
+        return Objects.equals(id, worker.id) && Objects.equals(name, worker.name) && Objects.equals(dailyIncome, worker.dailyIncome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, dailyIncome);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getDailyIncome() {
+        return dailyIncome;
+    }
+
+    public void setDailyIncome(Double dailyIncome) {
+        this.dailyIncome = dailyIncome;
+    }
+}
--- hr-payroll/src/main/java/com/matheus/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/matheus/hrpayroll/services/PaymentService.java
@@ -1,12 +1,29 @@
 package com.matheus.hrpayroll.services;
 
 import com.matheus.hrpayroll.enteties.Payment;
+import com.matheus.hrpayroll.enteties.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Value("${hr-worker.host}")
+    private String workerHost;
 
     public Payment getPayment(long workerId, int days){
-        return new Payment("Bob", 200.0, days);
+
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("id", ""+workerId);
+        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}",
+                Worker.class, uriVariables);
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
